95591afe03b7143b06040577cfc7e9e91287f152
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-02 20:38:18 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -9,6 +9,6 @@ class Song < ActiveRecord::Base
   def drake_made_this
     binding.pry
     # when this method is called it should assign the song's artist to Drake
-    self.artist = 
+    self.artist
   end
 end
